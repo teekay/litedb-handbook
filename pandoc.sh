a88cfb525cf268dc9cb1e2d96200033eec15697e
teekay <tomas.kohl@gmail.com>
2022-10-06 13:35:41 +0200

    Experiment with lualatex (fail)

--- pandoc.sh
+++ pandoc.sh
@@ -1,1 +1,1 @@
-pandoc -f markdown -V linkcolor:blue -V geometry:a4paper -V geometry:margin=1cm -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono" --pdf-engine=xelatex src/markdown/*.md -o dist/Handbook.pdf
+pandoc -f markdown -V linkcolor:blue -V geometry:a4paper -V geometry:margin=1cm -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono" --pdf-engine=lualatex src/markdown/*.md -o dist/Handbook.pdf
