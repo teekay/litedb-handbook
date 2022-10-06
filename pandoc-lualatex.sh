aead27e2d83d45975f24e511335facf582fc0970
teekay <tomas.kohl@gmail.com>
2022-10-06 13:36:59 +0200

    Refactor pandoc shell script

---
+++ pandoc-lualatex.sh
@@ -0,0 +1,7 @@
+pandoc -f markdown \
+-V linkcolor:blue \
+-V geometry:a4paper \
+-V geometry:margin=1cm \
+-V mainfont="DejaVu Serif" \
+-V monofont="DejaVu Sans Mono" \
+--pdf-engine=lualatex src/markdown/*.md -o dist/Handbook.pdf
--- pandoc.sh
+++
@@ -1,1 +0,0 @@
-pandoc -f markdown -V linkcolor:blue -V geometry:a4paper -V geometry:margin=1cm -V mainfont="DejaVu Serif" -V monofont="DejaVu Sans Mono" --pdf-engine=lualatex src/markdown/*.md -o dist/Handbook.pdf
